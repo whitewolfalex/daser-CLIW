6ac74d9b62513801e78787141441847b64a4b87e
Marian Alexandru Daniel <codrinalex1994@yahoo.com>
2018-11-02 00:39:21 +0200

    export svg functionality prototype
    All needs to be visible in the viewBox in order to be exported well. (research how to export without all being visible in the viewBox)

--- javascript/svg-zoom.js
+++ javascript/svg-zoom.js
@@ -2,13 +2,15 @@ var SvgGroup = document.getElementById("group-svg-objects");
 var SvgGroupSize = 1;
 var SvgGroupVariation = 0.1;
 
-function zoomIn(){
+function zoomIn()
+{
     SvgGroupSize += SvgGroupVariation;
     SvgGroup.style.transform = "scale("+ SvgGroupSize +")";
 }
 
 
-function zoomOut(){
+function zoomOut()
+{
     SvgGroupSize -= SvgGroupVariation;
     SvgGroup.style.transform = "scale("+ SvgGroupSize +")";
 }
