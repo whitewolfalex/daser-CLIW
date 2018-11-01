425f1b136c886bf336cd8c4b67736cf88e2070eb
Marian Alexandru Daniel <codrinalex1994@yahoo.com>
2018-11-02 00:25:29 +0200

    Zoom in and zoom out functionality

---
+++ javascript/svg-zoom.js
@@ -0,0 +1,14 @@
+var SvgGroup = document.getElementById("group-svg-objects");
+var SvgGroupSize = 1;
+var SvgGroupVariation = 0.1;
+
+function zoomIn(){
+    SvgGroupSize += SvgGroupVariation;
+    SvgGroup.style.transform = "scale("+ SvgGroupSize +")";
+}
+
+
+function zoomOut(){
+    SvgGroupSize -= SvgGroupVariation;
+    SvgGroup.style.transform = "scale("+ SvgGroupSize +")";
+}
