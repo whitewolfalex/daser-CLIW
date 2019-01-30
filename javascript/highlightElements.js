2c391ef866acf78cf9840792c46a97a45f644f51
Mada Lina <jitaru_mada@yahoo.com>
2019-01-30 21:37:27 +0200

    info how to use and highlights

---
+++ javascript/highlightElements.js
@@ -0,0 +1,7 @@
+function HighlightElementOn(idOfTheElement){
+    document.getElementById(idOfTheElement).style.boxShadow = "0px 0px 0px 4px red";
+}
+
+function HighlightElementOff(idOfTheElement){
+    document.getElementById(idOfTheElement).style.boxShadow = "none";
+}
